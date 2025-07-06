57dd52a668e9cdb55e5727eac40f709aa60cd593
LeftVankee <sethusai2007@gmail.com>
2025-07-06 17:16:56 +0530

    Hello

---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+public class BookRepository 
+{
+	int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		System.out.println("Setting ID.......");
+		this.id = id;
+	}
+
+	
+}
